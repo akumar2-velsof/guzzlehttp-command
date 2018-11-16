46d8a2da8e15c6cddf27c2d7ffd48ae332883dbe
mockingfish <inquiry@velsof.com>
2018-11-16 19:07:03 +0530

    Initial Commit

---
+++ src/Exception/CommandClientException.php
@@ -0,0 +1,7 @@
+<?php
+namespace GuzzleHttp1\Command\Exception;
+
+/**
+ * Exception encountered when a 4xx level response is received for a request
+ */
+class CommandClientException extends CommandException {}
---
+++ src/Exception/CommandServerException.php
@@ -0,0 +1,7 @@
+<?php
+namespace GuzzleHttp1\Command\Exception;
+
+/**
+ * Exception encountered when a 5xx level response is received for a request
+ */
+class CommandServerException extends CommandException {}
